f9f6861f9719e0b3cc2b279cd87242f89025dabf
Victor Oliveira <victor.oliveira@blueservice.com.br>
2022-06-15 12:35:35 -0300

    construção do serviço de autenticacao

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: Auth) { }
+
+  async register({email, password}) {
+    try{
+    const user = await createUserWithEmailAndPassword(this.auth, email, password);
+    return user;
+    } catch(e) {
+      return null;
+    }
+  }
+  async login({email, password}) {
+    try {
+      const user = await signInWithEmailAndPassword(this.auth, email,password);
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+  logout() {
+    return signOut(this.auth);
+  }
 }

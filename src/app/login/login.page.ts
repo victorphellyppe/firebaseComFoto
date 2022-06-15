4f74e1dc410fc5d8ff2a2efa068d517cc8e26747
Victor Oliveira <victor.oliveira@blueservice.com.br>
2022-06-15 13:34:27 -0300

    validação do campo email no formulario , duas formas de usar a função cadastrar

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,7 +8,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  credentials: FormGroup;
+  credentialsForm: FormGroup;
 
   constructor(
     private fb: FormBuilder,
@@ -17,24 +17,27 @@ export class LoginPage implements OnInit {
 
   // Fácil acesso para campos de formulário
 
-  getEmail() {
-    return this.credentials.get('email');
+  get Email() {
+    return this.credentialsForm.get('email');
   }
 
-  getPassword() {
-    return this.credentials.get('password');
+  get Password() {
+    return this.credentialsForm.get('password');
   }
   ngOnInit() {
-    this.credentials = this.fb.group({
-      email: ['', Validators.required, Validators.email],
-      password: ['', Validators.required, Validators.minLength(6)]
+    this.credentialsForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
+  get ErrorControl(){
+    return this.credentialsForm.controls;
+  }
   async register(){
     const loading = await this.utils.loadingCtrl.create();
     await loading.present();
-    const user = await this.utils.authSvc.register(this.credentials.value);
+    const user = await this.utils.authSvc.register(this.credentialsForm.value);
     await loading.dismiss();
 
     if (user) {
@@ -47,7 +50,7 @@ export class LoginPage implements OnInit {
   async login() {
     const loading = await this.utils.loadingCtrl.create();
     await loading.present();
-    const user = await this.utils.authSvc.login(this.credentials.value);
+    const user = await this.utils.authSvc.login(this.credentialsForm.value);
     await loading.dismiss();
 
     if (user) {

466b69fe973293cf3ca30a1af49294633202d27e
Victor Oliveira <victor.oliveira@blueservice.com.br>
2022-06-15 17:08:48 -0300

    colocando persistencia de dados no aparelho mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { environment } from '../environments/environment';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
+import { Capacitor } from '@capacitor/core';
+import { indexedDBLocalPersistence, initializeAuth } from 'firebase/auth';
+import { getApp } from 'firebase/app';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,7 +23,15 @@ import { provideStorage, getStorage } from '@angular/fire/storage';
     IonicModule.forRoot(),
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideAuth(() => getAuth()),
+    provideAuth(() => {
+      if(Capacitor.isNativePlatform()){
+        return initializeAuth(getApp(),{
+          persistence: indexedDBLocalPersistence
+        });
+      } else {
+        return getAuth();
+      }
+    }),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
   ],

d2c1c3c6a24608aabd5e90768ccfccbf16fbc125
Victor Oliveira <victor.oliveira@blueservice.com.br>
2022-06-20 18:48:18 -0300

    finalizando dia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,9 @@
+import {
+  PushNotifications,
+  PushNotificationSchema,
+  ActionPerformed,
+  Token,
+} from '@capacitor/push-notifications';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +12,49 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initializeFirebaseApp();
+  }
+
+  initializeFirebaseApp() {
+    console.log('Initializing Firebase on APP.COMPONENT');
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermissions().then((result) => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration', (token: Token) => {
+      alert('Push registration success, token: ' + token.value);
+    });
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
+  }
 }

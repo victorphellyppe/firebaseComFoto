e1a94a4e1e42bd0d91e1cf2706a7736aa322bf3c
Victor Oliveira <victor.oliveira@blueservice.com.br>
2022-06-23 18:30:40 -0300

    removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,11 +22,7 @@ const routes: Routes = [
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
     ...canActivate(redirectLoggedInToHome)
   },
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
-    ...canActivate(redirectUnauthorizedToLogin)
-  },
+
   {
     path: 'profile',
     loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule),
